753971d94bd5abf12af86442cd338c89e2a09cf4
Edward Chan <chankaward@gmail.com>
2015-01-07 18:36:55 +0800

    add postgresql_backup script

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ## Usage
 
 ```sh
+# MySQL
 sh -c "`curl -fsSL https://raw.githubusercontent.com/chankaward/mysql_db_backup/master/backup_db.sh`"
+
+# PostgreSQL
+sh -c "`curl -fsSL https://raw.githubusercontent.com/chankaward/database_backup_script/master/postgresql_backup.sh`"
 ```
 
 ## License
---
+++ postgresql_backup.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+read -p "Enter your project folder name: " PROJECT
+read -p "Enter your project database name: " PROJECT_DB
+read -p "Enter mysql username: " MYSQL_USER
+read -p "Enter mysql password: " MYSQL_PASSWORD
+
+gem install backup
+
+mkdir -p /var/www/$PROJECT/backup /var/www/$PROJECT/backup/db
+
+cd ~
+
+mkdir Backup
+
+cd Backup
+
+backup generate:model -t $PROJECT_DB --databases=mysql --compressor=gzip --storages=local
+
+cd models
+
+sudo bash -c "echo -e '
+# encoding: utf-8
+
+Model.new(:$PROJECT_DB, \"Dump $PROJECT_DB database\") do
+
+  ##
+  # PostgreSQL [Database]
+  #
+  database PostgreSQL do |db|
+    # To dump all databases, set `db.name = :all` (or leave blank)
+    db.name               = \"$PROJECT_DB\"
+    db.username           = \"$MYSQL_USER\"
+    db.password           = \"$MYSQL_PASSWORD\"
+    db.host               = \"localhost\"
+    db.port               = 5432
+    # db.socket             = "/tmp/pg.sock"
+    # When dumping all databases, `skip_tables` and `only_tables` are ignored.
+    # db.skip_tables        = ["skip", "these", "tables"]
+    # db.only_tables        = ["only", "these", "tables"]
+    db.additional_options = ["-xc", "-E=utf8"]
+  end
+
+  ##
+  # Local (Copy) [Storage]
+  #
+  store_with Local do |local|
+    local.path       = \"/var/www/$PROJECT/backup/db\"
+    local.keep       = 7
+  end
+
+  ##
+  # Gzip [Compressor]
+  #
+  compress_with Gzip
+
+end
+
+' > $PROJECT_DB.rb"
+
+backup perform -t $PROJECT_DB
+
+# Use whenever to automatic backup database
+gem install whenever
+
+cd /var/www/$PROJECT
+
+mkdir config
+
+wheneverize .
+
+sudo bash -c "echo -e '
+set :output, \"~/Backup/${PROJECT}"_whenever.log"\"
+# Here to set up auto backup database once a day
+# More info please see https://github.com/javan/whenever#example-schedulerb-file
+every :day do
+  command \"cd ~/Backup && backup perform -t $PROJECT_DB\"
+end
+' > config/schedule.rb"
+
+whenever --update-crontab
